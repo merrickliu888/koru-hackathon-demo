544814585ae26c4bc9635f37d63830c2246f042f
Merrick Liu <merrickliu168@gmail.com>
2025-02-05 21:36:58 -0500

    backend main.py

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+import fastapi
+from starlette.middleware.cors import CORSMiddleware
+
+app = fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
