ab84e2797a6655ad201a2935640c44b2677eb448
Merrick Liu <merrickliu168@gmail.com>
2025-02-12 21:10:25 -0500

    chat

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,9 @@ from google import genai
 from cohere import ClientV2 as CohereClient
 from clients import inject_google_genai_client, inject_cohere_client
 from llm import process_images, generate_form
+from constants_and_types import ChatRequest
+from fastapi.responses import StreamingResponse
+import json
 
 app = fastapi.FastAPI()
 
@@ -15,6 +18,30 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.post("chat")
+async def chat(request: ChatRequest, cohere_client: CohereClient = Depends(inject_cohere_client)):
+    messages = request.messages
+    lesson_plan = request.lesson_plan
+    user_prompt = messages[-1].content
+    system_prompt = {
+        "role": "system",
+        "content": "You are a helpful assistant that can answer questions and help with tasks. Your goal is to help the user understand the contents of a lesson plan."
+    }
+    modified_prompt = "Here is the lesson plan: " + json.dumps(lesson_plan) + "\n\n" + "Here is the user's prompt: " + user_prompt
+    messages[-1].content = modified_prompt
+    messages = [system_prompt] + messages
+
+    async def stream_response():
+        response = await cohere_client.chat_stream(
+            model="command-r-plus",
+            messages=messages,
+        )
+        async for chunk in response:
+            if chunk.type == "content-delta":
+                yield chunk.text
+
+    return StreamingResponse(stream_response(), media_type="text/event-stream")
 
 @app.post("generate-lesson-plan")
 async def generate_lesson_plan(request: Request, 
